ae5c45aaf830e003317605d43266b118bb6b6469
IsaacAbra <isaac.abramov03@gmail.com>
2020-06-11 16:48:33 -0400

    here it is

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,14 +2,9 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-const events = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
-  $('#sign-up').on('submit', events.onSignUp)
-  $('#sign-in').on('submit', events.onSignIn)
-  $('#change-password').on('submit', events.onChangePassword)
-  $('#sign-out').on('submit', events.onSignOut)
 })
